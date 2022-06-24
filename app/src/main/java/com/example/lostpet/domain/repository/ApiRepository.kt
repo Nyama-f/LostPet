d89305b025a14c785cc614e3fe24d2c427ea49ff
Nyama-f <74872487+Nyama-f@users.noreply.github.com>
2022-06-24 16:12:04 +0500

    Реализация Data Domain. Для дальнейшей реализации нужен DI. Сделаю в отдельной ветке его

---
+++ app/src/main/java/com/example/lostpet/domain/repository/ApiRepository.kt
@@ -0,0 +1,20 @@
+package com.example.lostpet.domain.repository
+
+import com.example.lostpet.data.model.Pet
+import com.example.lostpet.data.model.User
+import kotlinx.coroutines.flow.Flow
+
+interface ApiRepository {
+
+    fun getUsers(): Flow<List<User>>
+
+    fun getUser(userId: Int): Flow<User>
+
+    fun addUser(user: User)
+
+    fun getPets(): Flow<List<Pet>>
+
+    fun getPet(petId: Int): Flow<Pet>
+
+    fun addPet(pet: Pet)
+}
