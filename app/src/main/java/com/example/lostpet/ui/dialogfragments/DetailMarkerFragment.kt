b2d207e1fb6fefd2422fc91e31757f47936d3108
Nyama-f <74872487+Nyama-f@users.noreply.github.com>
2022-07-01 12:13:40 +0500

    Раскрывающийся BottomSheetDialogFragment.kt

--- app/src/main/java/com/example/lostpet/ui/dialogfragments/DetailMarkerFragment.kt
+++ app/src/main/java/com/example/lostpet/ui/dialogfragments/DetailMarkerFragment.kt
@@ -1,16 +1,16 @@
 package com.example.lostpet.ui.dialogfragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.lostpet.R
+import android.widget.FrameLayout
 import com.example.lostpet.data.model.Pet
 import com.example.lostpet.databinding.FragmentDetailMarkerBinding
-import com.example.lostpet.databinding.FragmentMapsBinding
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
+private const val COLLAPSED_HEIGHT = 228
 
 class DetailMarkerFragment : BottomSheetDialogFragment() {
 
@@ -22,6 +22,51 @@ class DetailMarkerFragment : BottomSheetDialogFragment() {
     }
 
 
+    override fun onStart() {
+        super.onStart()
+
+        val density = requireContext().resources.displayMetrics.density
+
+        dialog?.let {
+            val bottomSheet = it.findViewById<View>(com.google.android.material.R.id.design_bottom_sheet) as FrameLayout
+            val behavior = BottomSheetBehavior.from(bottomSheet)
+
+            // Выставляем высоту для состояния collapsed и выставляем состояние collapsed
+            behavior.peekHeight = (COLLAPSED_HEIGHT * density).toInt()
+            behavior.state = BottomSheetBehavior.STATE_COLLAPSED
+
+            behavior.addBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback() {
+
+                override fun onStateChanged(bottomSheet: View, newState: Int) {
+                    // Нам не нужны действия по этому колбеку
+                }
+
+                override fun onSlide(bottomSheet: View, slideOffset: Float) {
+                    with(binding) {
+                        // Нас интересует только положительный оффсет, тк при отрицательном нас устроит стандартное поведение - скрытие фрагмента
+                        if (slideOffset > 0) {
+                            // Делаем "свёрнутый" layout более прозрачным
+                            layoutCollapsed.alpha = 1 - 2 * slideOffset
+                            // И в то же время делаем "расширенный layout" менее прозрачным
+                            layoutExpanded.alpha = slideOffset * slideOffset
+
+                            // Когда оффсет превышает половину, мы скрываем collapsed layout и делаем видимым expanded
+                            if (slideOffset > 0.5) {
+                                layoutCollapsed.visibility = View.GONE
+                                layoutExpanded.visibility = View.VISIBLE
+                            }
+
+                            // Если же оффсет меньше половины, а expanded layout всё ещё виден, то нужно скрывать его и показывать collapsed
+                            if (slideOffset < 0.5 && binding.layoutExpanded.visibility == View.VISIBLE) {
+                                layoutCollapsed.visibility = View.VISIBLE
+                                layoutExpanded.visibility = View.INVISIBLE
+                            }
+                        }
+                    }
+                }
+            })
+        }
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -38,6 +83,8 @@ class DetailMarkerFragment : BottomSheetDialogFragment() {
             textAuthorPet.text = pet?.petUserId.toString()
         }
     }
+
+
 
     /*
     Доделать BottomSheetFragment(сделать закругленные углы + добавить кнопки
